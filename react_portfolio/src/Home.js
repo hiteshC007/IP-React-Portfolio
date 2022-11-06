bcd8f4f345b2dfc2d10aef88ca55d6ee6205c29f
hiteshC007 <113360534+hiteshC007@users.noreply.github.com>
2022-11-06 14:13:22 +0530

    Add files via upload

---
+++ react_portfolio/src/Home.js
@@ -0,0 +1,26 @@
+import React from 'react'
+import './Home.css'
+import profilePic from './assets/profilePic.jpeg'
+
+const Home = () => {
+   
+    return (
+        <div className='home'>
+        <div className="homeMain">
+            <div className="homeName">
+               Hitesh Choudhary
+            </div>
+            <div className="homeImage">
+                <div className="imageContainer">
+                <img src={profilePic} alt="" />
+                </div>
+            </div>
+            <div className="homeDev">
+                I'm Hitesh from India ,a 20 year old tech enthusiast. I do content on Development. I really enjoy learning languages and frameworks like React ,as well as work in Frontend💻
+            </div>
+        </div>
+        </div>
+    )
+}
+
+export default Home
